2b622061b225b127f633010592f715635bd1b837
TMV <m.timarsuev@gmail.com>
2023-01-14 16:51:04 +0300

    Initial setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={"HOME PAGE"} />
+        <Route path="/menu" element={"STORE MAIN PAGE"} />
+        <Route path="/menu/:category" element={"{CATEGORY} STORE PAGE"} />
+        <Route path="/dostavka" element={"SHIPMENT PAGE"} />
+        <Route path="/news" element={"SPECIAL OFFERS PAGE"} />
+        <Route path="/news/:special" element={"SINGLE SPECIAL OFFER PAGE"} />
+        <Route path="/about" element={"ABOUT PAGE"} />
+        <Route path="/contact" element={"CONTACT PAGE"} />
+        <Route path="*" element={"404 PAGE"} />
+      </Routes>
+    </Router>
   );
 }
 
