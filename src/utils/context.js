e6c91079823a472d10bd9773254f6494b3583481
TMV <m.timarsuev@gmail.com>
2023-03-20 17:23:42 +0300

    AddToCart works with Loged and Anonymous session

--- src/utils/context.js
+++ src/utils/context.js
@@ -52,8 +52,77 @@ export const AppContextProvider = ({ children }) => {
     dispatch({ type: "STOP_LOADING" });
   };
 
-  const addToCart = (packageInfo) =>
-    dispatch({ type: "ADD_TO_CART", payload: packageInfo });
+  const addToCart = async (cartItemInfo) => {
+    // If no user -> add to anonymous cart
+    if (!state.user) {
+      const cart = addToAnonymousCart(cartItemInfo);
+
+      // Set updated cart
+      setCart(cart);
+    } else {
+      // If user exists -> add to db
+      const userCart = await addToUserCart(cartItemInfo);
+
+      if (!userCart) return;
+
+      // Set updated cart
+      setCart(userCart);
+    }
+  };
+
+  function addToAnonymousCart(cartItemInfo) {
+    // Get item data
+    const { product, variant, amount } = cartItemInfo;
+
+    // Clone the cart to prevent changes in the state variable
+    const newCart = state.cart.map((cartItem) => {
+      return { ...cartItem };
+    });
+
+    // Find if item already in the cart
+    const items = newCart.filter(
+      (cartItem) => cartItem.product === product && cartItem.variant === variant
+    );
+
+    // If no items found -> add item to cart
+    if (items.length === 0) {
+      const extendedCart = [...state.cart, cartItemInfo];
+      // Write cart data to the session storage
+      sessionStorage.setItem("cart", JSON.stringify(extendedCart));
+
+      return extendedCart;
+    }
+
+    // If item already exists -> just increment the amount by reference
+    items[0].amount += amount;
+
+    // Write cart data to the session storage
+    sessionStorage.setItem("cart", JSON.stringify(newCart));
+
+    return newCart;
+  }
+
+  async function addToUserCart(cartItemInfo) {
+    const controller = new AbortController();
+
+    try {
+      const response = await fetch(`/api/v1/users/addToCart/${state.user.id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cartItemInfo),
+        signal: controller.signal,
+      });
+
+      const data = await response.json();
+
+      return data.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
 
   const clearCart = () => dispatch({ type: "CLEAR_CART" });
 
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -6,39 +6,6 @@ const reducer = (state, action) => {
     case "CLEAR_CART": {
       return { ...state, cart: [] };
     }
-    case "ADD_TO_CART": {
-      // Get item data
-      const { product, variant, amount } = action.payload;
-
-      // Clone the cart to prevent changes in the state variable
-      const newCart = state.cart.map((cartItem) => {
-        return { ...cartItem };
-      });
-
-      // Find if item already in the cart
-      const items = newCart.filter(
-        (cartItem) =>
-          cartItem.product === product && cartItem.variant === variant
-      );
-
-      // If no items found -> add item to cart
-      if (items.length === 0) {
-        const extendedCart = [...state.cart, action.payload];
-        // Write cart data to the session storage
-        sessionStorage.setItem("cart", JSON.stringify(extendedCart));
-
-        return { ...state, cart: extendedCart };
-      }
-
-      // If item already exists -> just increment the amount by reference
-      items[0].amount += amount;
-
-      // Write cart data to the session storage
-      sessionStorage.setItem("cart", JSON.stringify(newCart));
-
-      // Set new cart
-      return { ...state, cart: [...newCart] };
-    }
     case "START_LOADING": {
       return { ...state, isLoading: true };
     }
